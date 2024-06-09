5784f2ca14efa35988ab278a6dc3d1cca5018475
Salman Hossen <hossainsalman873@gmail.com>
2024-06-09 11:09:11 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Accordion from './Accordion'; // Correctly import Accordion
+
+const App = () => {
+  return (
+    <div>
+      <h1>My App</h1>
+      <Accordion />
+    </div>
+  );
+};
+
+export default App;
